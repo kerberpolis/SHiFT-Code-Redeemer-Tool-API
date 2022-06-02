7931d98fd62603f1067ae82d1f99b0525cad9b31
Daniel Sutton - BGS <dsut@bgs.ac.uk>
2022-06-02 20:44:34 +0100

    rename function

--- app/input_borderlands_codes.py
+++ app/input_borderlands_codes.py
@@ -13,45 +13,44 @@ db_conn = database_controller.create_connection(database)
 
 
 def input_borderlands_codes(conn, user, codes):
-    if codes:
-        logged_in_borderlands = False
-        crawler = dtc.BorderlandsCrawler(user)
+    logged_in_borderlands = False
+    crawler = dtc.BorderlandsCrawler(user)
 
-        for row in codes:
-            code = row[3]
-            code_type = row[4]
-            expiry_date = row[7]
+    for row in codes:
+        code = row[3]
+        code_type = row[4]
+        expiry_date = row[7]
 
-            if expiry_date:  # allow all keys for now, even if supposedly expired, may still be redeemable
-                try:
-                    if not logged_in_borderlands:
-                        crawler.login_gearbox()
-                        logged_in_borderlands = True
-                    result = crawler.input_code_gearbox(code)
+        if expiry_date:  # allow all keys for now, even if supposedly expired, may still be redeemable
+            try:
+                if not logged_in_borderlands:
+                    crawler.login_gearbox()
+                    logged_in_borderlands = True
+                result = crawler.input_code_gearbox(code)
 
-                    if result:
-                        logging.info(f'Redeemed {code_type} code {code}')
-                        # update expired attribute in codes table
-                        database_controller.update_invalid_code(conn, row[0])
-                except GearBoxError:
-                    logging.info(f'There was an error with gearbox when redeeming code {row[0]}, {code}')
-                except ConsoleOptionNotFoundException as e:
-                    logging.info(str(e))
+                if result:
+                    logging.info(f'Redeemed {code_type} code {code}')
+                    # update expired attribute in codes table
                     database_controller.update_invalid_code(conn, row[0])
-                except GameNotFoundException as e:
-                    logging.info(f'Code {code} cannot be redeemed as code is for a game '
-                                 f'you do not own. {str(e)} game is not valid ')
-                    database_controller.update_invalid_code(conn, row[0])
-                except CodeFailedException as e:
-                    logging.info(str(e))
-                    database_controller.update_invalid_code(conn, row[0])
-                except Exception:
-                    pass
-            else:
+            except GearBoxError:
+                logging.info(f'There was an error with gearbox when redeeming code {row[0]}, {code}')
+            except ConsoleOptionNotFoundException as e:
+                logging.info(str(e))
                 database_controller.update_invalid_code(conn, row[0])
-                print(f'This {code_type} code: {code}, has expired')
+            except GameNotFoundException as e:
+                logging.info(f'Code {code} cannot be redeemed as code is for a game '
+                             f'you do not own. {str(e)} game is not valid ')
+                database_controller.update_invalid_code(conn, row[0])
+            except CodeFailedException as e:
+                logging.info(str(e))
+                database_controller.update_invalid_code(conn, row[0])
+            except Exception:
+                pass
+        else:
+            database_controller.update_invalid_code(conn, row[0])
+            print(f'This {code_type} code: {code}, has expired')
 
-        crawler.tear_down()
+    crawler.tear_down()
 
 
 def setup_logger():
@@ -81,19 +80,21 @@ def setup_tables(conn):
         print("Error! cannot create the database connection.")
 
 
-def start_threads(conn):
+def start_crawlers(conn):
     valid_codes = database_controller.select_valid_codes(conn)
-    for user in database_controller.select_all_users(conn):
-        thread = threading.Thread(target=input_borderlands_codes,
-                                  name=f"borderlands_input_{user[0]}",
-                                  args=(conn, user, valid_codes))
-        thread.start()
-        print(f'{thread.name} has started')
+
+    if valid_codes:
+        for user in database_controller.select_all_users(conn):
+            thread = threading.Thread(target=input_borderlands_codes,
+                                      name=f"borderlands_input_{user[0]}",
+                                      args=(conn, user, valid_codes))
+            thread.start()
+            print(f'{thread.name} has started')
 
 
 if __name__ == "__main__":
     setup_logger()
     setup_tables(db_conn)
-    start_threads(db_conn)
+    start_crawlers(db_conn)
 
 
